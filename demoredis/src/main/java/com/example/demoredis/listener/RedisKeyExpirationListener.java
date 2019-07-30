970e4f787be9bccb5d7d7b6dad854a2bb01fdf3e
Zhang.Ye <Zhang.Ye@pb.com>
2019-07-30 17:17:26 +0800

    Add a listener for expired key.

---
+++ demoredis/src/main/java/com/example/demoredis/listener/RedisKeyExpirationListener.java
@@ -0,0 +1,34 @@
+package com.example.demoredis.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+    private final static Logger logger = LoggerFactory.getLogger(RedisKeyExpirationListener.class);
+
+    /**
+     * Creates new {@link RedisKeyExpirationListener} for {@code __keyevent@*__:expired} messages.
+     *
+     * @param listenerContainer must not be {@literal null}.
+     */
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        // 用户做自己的业务处理即可,注意message.toString()可以获取失效的key
+        String expiredKey = message.toString();
+//        if (expiredKey.startsWith("zeus:order")) {
+//            //TODO
+//        }
+        logger.info("############################################ "+expiredKey);
+    }
+}
+
